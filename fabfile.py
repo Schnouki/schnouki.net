d318d31f724a9e86d0fac57a1f88894430494375
Thomas Jost <schnouki@schnouki.net>
2015-01-14 18:16:05 +0100

    fabfile: add livereload

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,12 @@
 from fabric.api import *
+from fabric.context_managers import lcd
 import fabric.contrib.project as project
 import os
 import sys
 import SimpleHTTPServer
 import SocketServer
+
+import livereload
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
@@ -44,6 +47,26 @@ def serve(port=9090):
 def reserve(port=9090):
     build()
     serve(port)
+
+def live(port=9090):
+    clean()
+    build()
+
+    orig_dir = os.getcwd()
+    os.chdir(env.deploy_path)
+
+    def live_rebuild():
+        with lcd(orig_dir):
+            build()
+    server = livereload.Server()
+    server.watch('../content', live_rebuild)
+    server.watch('../my_plugins', live_rebuild)
+    server.watch('../plugins', live_rebuild)
+    server.watch('../themes', live_rebuild)
+    server.watch('*.css')
+    server.watch('*.html')
+    server.watch('*.js')
+    server.serve(liveport=35729, port=int(port))
 
 def preview():
     local('pelican -s publishconf.py')
